153ae4b5562b532c469083b1fd5bdef69fbd1c79
Philip Erickson <perickson@mavenlink.com>
2021-05-01 12:55:33 -0600

    Refactor workflow inspector
    At first i wasn't sure if the inspector should even exist; i thought maybe those methods should just be on the workflow directly. however, i think this is a nicer separation of concerns--inspecting state isn't really the workflow's single responsibility. Plus, having the inspector as a separate class makes it easier to inherit from or compose or decorate or whatever if people want to provide their own inspection methods on top of what the base inspector provides

--- lib/gruffle/inspector.rb
+++ lib/gruffle/inspector.rb
@@ -2,17 +2,35 @@ require 'gruffle/workflow_states'
 
 module Gruffle
   class Inspector
-    def initialize(workflow_instance) # TODO: take either instance or class
-      @workflow_instance = workflow_instance
+    def initialize(workflow)
+      @workflow, @workflow_class = initialize_workflow(workflow)
+      @state_store = @workflow.send(:state_store)
     end
 
-    def states
-      @workflow_instance.states
-    end
-
+    # TODO: re think this method...
+    # There's not generally just one initial state on a workflow. There'd be 1 per exeuction of that workflow. Should
+    # the inspector have chainable methods that let you filter down to a particular execution, for example?
     def initial_state
-      @workflow_instance.initial_state
+      initial_class = @workflow_class.states(:initial).keys.first
+      initial_states = @state_store.by_class(initial_class)
+      initial_states.find { |s| s.trace.empty? }
     end
 
+    def final_states
+      final_classes = @workflow_class.states(:final).keys.first
+      @state_store.by_class(*final_classes)
+    end
+
+    private
+
+    def initialize_workflow(workflow)
+      if workflow.is_a? Gruffle::Workflow
+        return workflow, workflow.class
+      elsif workflow.respond_to?(:<) && workflow < Gruffle::Workflow
+        return workflow.new, workflow
+      else
+        raise "Unable to inspect workflow: #{workflow}"
+      end
+    end
   end
 end
--- spec/gruffle/inspector_spec.rb
+++ spec/gruffle/inspector_spec.rb
@@ -6,15 +6,27 @@ describe Gruffle::Inspector do
   let(:dummy_workflow) { DummyWorkflow.new }
   let(:inspector) { Gruffle::Inspector.new(dummy_workflow) }
 
-  describe '#states' do
-    it 'provides read access to the workflow states' do
-      expect(inspector).to respond_to :states
+  describe '#initialize' do
+    it 'can be initialized with a workflow class' do
+      i = Gruffle::Inspector.new(dummy_workflow.class)
+      expect { i.initial_state }.not_to raise_error
+    end
+
+    it 'can be initialized with a workflow instance' do
+      i = Gruffle::Inspector.new(dummy_workflow)
+      expect { i.initial_state }.not_to raise_error
     end
   end
 
   describe '#initial_state' do
-    it 'provides read access to the workflow initial state' do
+    it 'provides read access to the workflow states' do
       expect(inspector).to respond_to :initial_state
+    end
+  end
+
+  describe '#final_states' do
+    it 'provides read access to the workflow initial state' do
+      expect(inspector).to respond_to :final_states
     end
   end
 end
